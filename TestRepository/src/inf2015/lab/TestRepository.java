5fc8c57d95ddf7fad2e3506ff6ca7f6b5e1319a2
Patrick Boudreau <patrick@lpcmedia.com>
2011-09-29 21:02:29 -0400

    Started GitHub

---
+++ TestRepository/src/inf2015/lab/TestRepository.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf2015.lab;
+
+/**
+ *
+ * @author patrick
+ */
+public class TestRepository {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Git test 1");
+    }
+}
